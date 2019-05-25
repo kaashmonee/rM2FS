619082a8f4e8ca9014fd9c6eae4cabb2d6b3adf6
skanda <skandakk@gmail.com>
2019-05-24 19:43:53 -0700

    Writing some helper functions

--- test.py
+++ test.py
@@ -7,17 +7,46 @@ import matplotlib.pyplot as plt
 """
 Steps:
 1. Open .fits file to view in Python.
+2. 
 """
 def open_fits():
     hdul = fits.open("fits_files/b0759_stitched.fits")
-    print(hdul)
-    print(hdul.info())
     image_data = hdul[0].data
-    print(image_data.shape)
-    print(type(image_data))
     plt.imshow(image_data, cmap="gray")
     plt.colorbar()
-    print("Got here!")
+    # print("Got here!")
     plt.show()
 
 open_fits()
+
+
+def open_fits_aplpy():
+    gc = aplpy.FITSFigure("fits_files/b0759_stitched.fits")
+
+
+class ContinuumCreator:
+    def __init__(self, fits_file):
+        """
+        Creates a class that opens and contains .fits image attributes. It takes in a fits image path.
+        """
+        self.hdul = fits.open(fits_file)
+        self.image_data = self.hdul[0].data
+
+    def open_image(self):
+        """
+        Opens the .fits image for viewing. This is for testing purposes to ensure that the image opened is correct. 
+        """
+        plt.imshow(self.image_data, cmap="gray")
+        plt.colorbar()
+        plt.show()
+
+
+def main():
+    """
+    Main routine for testing purposes..
+    """
+
+
+     
+if __name__ == "__main__":
+    main()
