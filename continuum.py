82b7c6e971137b7a51620b3dd8348fe412da508d
skanda <skandakk@gmail.com>
2019-05-28 23:13:54 -0700

    Obtained flux from a particular column xpixel value

--- continuum.py
+++ continuum.py
@@ -11,6 +11,27 @@ from fitsfile import FitsFile
 
 
 
+# Pick an x pixel to plot flux
+# Find intensity of y pixels
+# Plot intensity
+# Obtain local maxima
+# local maxima is the y value at which the continuum exists.
+# rinse and repeat for a few more spectra and we are good 
 
 
+def find_flux(image):
+    xpixel = 1000
+    intensity = []
+    for row_num, row in enumerate(image):
+        intensity.append((row_num, row[xpixel]))
 
+    print(intensity)
+
+
+def main():
+    fits_file = FitsFile("fits_files/r0760_stitched.fits")
+    image = fits_file.image_data
+    find_flux(image)
+
+if __name__ == "__main__":
+    main()
