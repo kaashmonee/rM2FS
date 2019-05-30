d3694c1aae995ed9ca31c6b3d4e7e9b2a0c61f20
skanda <skandakk@gmail.com>
2019-05-30 14:06:03 -0700

    Add picture for documentation purposes

--- continuum.py
+++ continuum.py
@@ -62,6 +62,9 @@ def find_peaks(intensity_array):
 
     # Makes a scatter plot of the location of the peaks (peaks[0]) and
     # the intensity value of the peaks (intensity_array[peaks[0]])
+    plt.xlabel("ypixel")
+    plt.ylabel("intensity")
+    plt.title("intensity v. ypixel with peaks shown")
     plt.scatter(peaks[0], intensity_array[peaks[0]], color="red")
     plt.show()
 
