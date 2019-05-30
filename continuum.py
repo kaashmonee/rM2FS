54eeeb94a0dd96719bb7d39db111af0d01f10a1d
skanda <skandakk@gmail.com>
2019-05-29 22:05:39 -0700

    Rewrite to include a bin implementation. Create several bins and determine where the average changes. That y pixel is the boundary of the order

--- continuum.py
+++ continuum.py
@@ -58,20 +58,30 @@ def find_order_boundaries(intensity_array):
     Storing this x value will be important as we will be using these routines again to 
     do the same thing.
     """
-    lower_flux_limit = 10
+    num_bins = 1000
+
+    index_array = np.arange(len(intensity_array))
+    index_bins = np.array_split(index_array, num_bins)
+
     intensity_array = np.array(intensity_array)
-    intensity_array_shifted_down = intensity_array - lower_flux_limit
+    bins = np.array_split(intensity_array, num_bins)
+    bin_averages = [np.average(bin) for bin in bins]
+
+    plt.plot(bin_averages, "ro")
+
+    # plt.plot(intensity_array)
+    # intensity_array_shifted_down = intensity_array - lower_flux_limit
 
     # obtains the indices where a sign change occured
     # obtained from https://stackoverflow.com/questions/2652368/how-to-detect-a-sign-change-for-elements-in-a-numpy-array.
-    sign_change_array = np.where(np.sign(intensity_array_shifted_down[:-1]) != np.sign(intensity_array_shifted_down[1:]))[0] + 1
+    # sign_change_array = np.where(np.sign(intensity_array_shifted_down[:-1]) != np.sign(intensity_array_shifted_down[1:]))[0] + 1
 
-    print("intensity_array:", intensity_array)
-    print("sign_change_array:", sign_change_array)
+    # print("intensity_array:", intensity_array)
+    # print("sign_change_array:", sign_change_array)
 
-    plt.plot(intensity_array_shifted_down)
-    for y in sign_change_array:
-        plt.axvline(y, ymin=0, ymax=3000, color="red")
+    # plt.plot(intensity_array_shifted_down)
+    # for y in sign_change_array:
+    #     plt.axvline(y, ymin=0, ymax=3000, color="red")
 
     plt.show()
 
