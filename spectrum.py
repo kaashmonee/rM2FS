cf56f967a7b05c01cb8cab502d38a92c642cd323
skanda <skandakk@gmail.com>
2019-06-10 16:00:03 -0700

    Moved functions over to another class

--- spectrum.py
+++ spectrum.py
@@ -58,64 +58,6 @@ class Spectrum:
         return fit_plot
 
     
-    def find_true_centers(self):
-        """
-        This function finds the true center for each peak in the spectrum.
-        """
-        # Each element in this list contains a list of points to 
-        # fit to the gaussian
-        gfitpoints = []
-        for xvalue, ypeak in zip(self.xvalues, self.yvalues):
-            fit_points = self.__get_gaussian_coords(xvalue, ypeak)
-            gfitpoints.append(fit_points)
-
-        # Fit a gaussian
-        fitted_models = self.__fit_gaussian(self.xvalues, gfitpoints)
-        centers = [max(fit) for fit in fitted_models]
-
-        return centers
-
-    def __fit_gaussian(self, xvalues, gfitpoints):
-        """
-        Fits a gaussian to each distribution of xvalues and points in each gfitpoints.
-        Obtained from 
-        https://stackoverflow.com/questions/44480137/how-can-i-fit-a-gaussian-curve-in-python. 
-        """
-        fitter = modeling.fitting.LevMarLSQFitter()
-        model = modeling.models.Gaussian1D()
-        fitted_models = [fitter(model, xvalues, points) for points in gfitpoints]
-
-        return fitted_models
-
-
-    def __get_gaussian_coords(xvalue, peak):
-        """
-        Finds the width of the peaks.
-        """
-
-        # do not include pixels dimmer than 100 to be the center of the gaussian
-        intensity_threshold = 100 
-        cur_y = peak # the current y pixel
-        gypixels = [] # y pixels to include in the gaussian fit
-
-        ## Finds all y pixels that are brigher than 100 in either direction.
-
-        gypixels.append(cur_y)
-
-        while self.image[xvalue, cur_y] > intensity_threshold:
-            cur_y -= 1
-            gypixels.append(cur_y)
-
-        cur_y = peak
-
-        while self.image[xvalue, cur_y] > intensity_threshold:
-            cur_y += 1
-            gypixels.append(cur_y)
-
-        gxpixels = np.repeat(xvalue, len(gypixels))
-        gypixels = np.array(gypixels)
-
-        return xpixels, gypixels
 
 
 
