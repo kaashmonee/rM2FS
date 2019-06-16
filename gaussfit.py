eb704371fe8db9d7b06da1dcf292d0e709ffcfcd
skanda <skandakk@gmail.com>
2019-06-16 12:12:39 -0700

    removed extraneous functions

--- gaussfit.py
+++ gaussfit.py
@@ -24,10 +24,6 @@ def get_true_peaks(fits_file):
 
     for spec_ind, spectrum in enumerate(fits_file.spectra):
         for peak in spectrum.peaks:
-            # y0 = get_previous_peak(fits_file, peak, spec_ind)
-            # y1 = peak.y
-            # y2 = get_next_peak(fits_file, peak, spec_ind)
-            # rng = get_range(y1-10, y1, y1+10, image_height)
             y1 = peak.y
             y0 = y1-3
             y2 = y1+3
@@ -83,45 +79,6 @@ def fit_gaussian(fits_file, rng, peak, spec_ind=0):
 
     peak.true_center = max(output)
             
-
-def get_previous_peak(fits_file, peak, spec_ind):
-    """
-    This function obtains the previous peak in the same column in the image so that
-    we can find the midpoint between the peaks to choose the y-domain for our 
-    Gaussian fit.
-    """
-
-    # If this is the first spectrum, then the previous peak is the start of the 
-    # picture.
-    if spec_ind == 0:
-        return 0
-
-    cur_spectrum = fits_file.spectra[spec_ind]
-    previous_spectrum = fits_file.spectra[spec_ind-1]
-    prev_peak_ind = np.where(np.array(previous_spectrum.xvalues) == peak.x)
-    ynminus1 = previous_spectrum.yvalues[prev_peak_ind[0][0]]
-
-    return ynminus1
-
-def get_next_peak(fits_file, peak, spec_ind):
-    """
-    This function obtains the next peak in the same column in the image so that
-    we can find the midpoint between the currnet peak and the next peak to 
-    choose a y-domain to fit a Gaussian.
-    """
-
-    # If this is the last spectrum, then the next peak is the end of the picture
-    if spec_ind == len(fits_file.spectra) - 1:
-        return fits_file.fits_image.shape[1]
-
-    cur_spectrum = fits_file.spectra[spec_ind]
-    next_spectrum = fits_file.spectra[spec_ind+1]
-    next_peak_ind = np.where(np.array(next_spectrum.xvalues) == peak.x)
-    yplus1 = next_spectrum.yvalues[next_peak_ind[0][0]]
-    
-
-    return yplus1
-
 # fits file
 # have spectra
 # obtain the peaks and the points adjacent to the peaks
