fa2df060ba259f88e0d2c1a77105b3700c2c6b09
skanda <skandakk@gmail.com>
2019-06-17 14:08:00 -0700

    Wrote and updated documentation

--- gaussfit.py
+++ gaussfit.py
@@ -47,18 +47,34 @@ def non_int_to_int(iterable):
     return [int(x) for x in iterable]
 
 def gauss(x, a, x0, sigma):
+    """
+    This is the Gaussian function. This takes the following parameters:
+    x: xvalues
+    a: yvalues
+    x0: ?
+    sigma: standard deviation
+    """
     return a*scipy.exp(-x*(x-x0)**2/(2*sigma**2))
 
 
 def fit_gaussian(fits_file, rng, peak, spec_ind=0):
+    """
+    This function obtains the fitting parameters for each Gaussian profile. 
+    This includes the mean, expected max, and the standard deviation. It then 
+    uses those parameters on a "continuous" domain to obtain a nice looking 
+    Gaussian from which we can obtain each peak.
+    """
     fits_image = fits_file.image_data
     
     # At a particular x value, this obtains the y values in the image so that 
     # we can get a set of points for which we want to fit the Gaussian.
-    ystart = rng[0] # rng[0]-200 if rng[0]-200 >= 0 else 0
-    yend = rng[1] # rng[1]+100 if rng[1]+100 <= fits_image.shape[1] else fits_file.image[1]
-    yrange = non_int_to_int(np.arange(ystart, yend))
-    peak.x = int(peak.x)
+    ystart = rng[0]     
+    yend = rng[1]
+
+    # Creates the the range of yvalues for which we want to fit our Gaussian    
+    yrange = np.arange(ystart, yend)
+
+    # Grabs the intensity at each y value and the given x value
     intensity = fits_image[yrange, peak.x]
     plt.scatter(yrange, intensity, color="red")
     
@@ -66,21 +82,27 @@ def fit_gaussian(fits_file, rng, peak, spec_ind=0):
     # safety check to ensure same number of my points
     assert(len(intensity) == len(yrange))
 
+    # x, y points for which we want to fit our Gaussian
+    x = np.array(yrange)
+    y = np.array(intensity)
 
-    ### I'm really not sure what a lot of this code here does. A lot of it is taken
-    # straight from stack overflow links that are cited here.
-    # it is taken from a lot of different online sources, and they are cited here:
-    n = len(intensity)
+    # We use a continuous domain as cited here so that we have a smooth Gaussian
+    # https://stackoverflow.com/questions/42026554/fitting-a-better-gaussian-to-data-points
+    x_continuous = np.linspace(yrange[0], yrange[-1], 100)
 
-    x = np.array(yrange); y = np.array(intensity)
-    xx = np.linspace(yrange[0], yrange[-1], 100)
-
+    # These are parameters used to construct the Gaussian.
+    # We divide by sum(y) for the reason cited here:
+    # https://stackoverflow.com/questions/44398770/python-gaussian-curve-fitting-gives-straight-line-supplied-amplitude-of-y
     peak_value = y.max()
     mean = sum(x*y)/sum(y)
     sigma = sum(y*(x-mean)**2)/sum(y)
 
-    popt, pcov = scipy.optimize.curve_fit(gauss, x, y, p0=[peak_value, mean, sigma], maxfev=5000)
-    plt.plot(xx, gauss(xx, *popt), label="fit")
+    # To determine the p0 values, we used the information here:
+    # https://stackoverflow.com/questions/29599227/fitting-a-gaussian-getting-a-straight-line-python-2-7.
+    popt, pcov = scipy.optimize.curve_fit(gauss, x, y, 
+                                      p0=[peak_value, mean, sigma], maxfev=5000)
+
+    plt.plot(x_continuous, gauss(x_continuous, *popt), label="fit")
 
     plt.show()
 
