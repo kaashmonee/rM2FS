f2b32c29025fff91e35e1bb5a42af2fcbaa80718
skanda <skandakk@gmail.com>
2019-06-20 14:48:52 -0700

    Removed get_spectra fromb eing publicly visible and ensured that all spectra would be gotten upon creating the FitsFile class. moved true peak finding to perform_fits

--- main.py
+++ main.py
@@ -34,10 +34,6 @@ def main():
         # TODO: ensure that the fits are still there, without going through the 
         # perform_fits pipeline.
         util.perform_fits(fits_file)
-        # util.save(fits_file)
-        # fits_file = util.load(fn + ".pkl")
-        # util.perform_fits(fits_file)
-        gaussfit.get_true_peaks(fits_file)
 
 
 
--- util.py
+++ util.py
@@ -28,7 +28,6 @@ def export_spectra(file_name, spectra):
     np.savetxt(file_name, polynomials, delimiter=",")
 
 def perform_fits(fits_file):
-    fits_file.get_spectra()
     # gaussfit.get_true_peaks(fits_file) # modifies the fits_file and saves each
     fits_file.plot_spectra(show=True)
 
