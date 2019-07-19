9ee100da88aaaa199a6dc8af73376d9591ac4aa8
skanda <skandakk@gmail.com>
2019-07-19 13:31:00 -0400

    Small style change in the bash script

--- svg_to_png.sh
+++ svg_to_png.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 FILES=completed_images/*
+save_directory="completed_image_png/"
+
 for f in $FILES
 do
     echo "Processing $f file..."
@@ -7,7 +9,7 @@ do
     basename "$f"
     filename="$(basename -- $f)"
     filename=$filename".png"
-    inkscape -z -e completed_images_png/$filename -w 1000 -h 1000 $f
+    inkscape -z -e $save_directory/$filename -w 1000 -h 1000 $f
     echo "Finished"
 done
 
