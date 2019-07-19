b6a381857c90ad59a27d403bab49449cef8d28d4
skanda <skandakk@gmail.com>
2019-07-19 13:12:00 -0400

    Small change

--- svg_to_png.sh
+++ svg_to_png.sh
@@ -4,7 +4,7 @@ for f in $FILES
 do
     echo "Processing $f file..."
     filename="$f.png"
-    inkscape -z -e completed_images_png/$filename
+    inkscape -z -e completed_images_png/$filename -w 1000 -h 1000 $f
     echo "Finished"
 done
 
