47f98b38e84700dc4fc596eec912bb8da5d10e1f
skanda <skandakk@gmail.com>
2019-07-22 13:49:04 -0400

    Updated svg_to_png script to take in cmd line arguments

--- svg_to_png.sh
+++ svg_to_png.sh
@@ -2,14 +2,27 @@
 FILES=completed_images/*
 save_directory="completed_images_png"
 
-for f in $FILES
-do
-    echo "Processing $f file..."
-    # Getting the extension
+if [ ! -z "$1" ]
+  then
+    echo "Processing $1 file..."
+    f="$1"
     basename "$f"
     filename="$(basename -- $f)"
     filename=$filename".png"
     inkscape -z -e $save_directory/$filename -w 1000 -h 1000 $f
-    echo "Finished"
-done
+fi
+
+if [ $# -eq 0 ]
+  then
+    for f in $FILES
+    do
+        echo "Processing $f file..."
+        # Getting the extension
+        basename "$f"
+        filename="$(basename -- $f)"
+        filename=$filename".png"
+        inkscape -z -e $save_directory/$filename -w 1000 -h 1000 $f
+        echo "Finished"
+    done
+fi
 
